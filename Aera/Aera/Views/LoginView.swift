350c61eb0cb56e99b8d6d32a721a7f5521dba70c
mzxxzysy <jsyjj21@gmail.com>
2025-11-21 17:58:22 +0900

    ✨ Feat: 로그인 퍼블리싱

--- Aera/Aera/Views/LoginView.swift
+++ Aera/Aera/Views/LoginView.swift
@@ -8,14 +8,104 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var shownPassword: Bool = false
+    
+    
     var body: some View {
         ZStack {
             Color.aricticLight
-
-            Text("LoginView")
-                .font(.XxlMedium)
+            
+            VStack{
+                Image(.logoDark)
+                    .padding(.bottom, 44)
+                InfoGroup
+                    .padding(.bottom, 24)
+                InputGroup
+                    .padding(.bottom, 32)
+                ButtonGroup
+            }
         }
         .ignoresSafeArea()
+    }
+    
+    private var InfoGroup: some View {
+        VStack(spacing: 8){
+            Text("로그인 하기")
+                .font(.MBold)
+            Text("아이디와 비밀번호를 입력하세요")
+                .font(.SMedium)
+        }
+    }
+    
+    private var InputGroup: some View {
+        VStack(spacing: 16){
+            InputView{
+                TextField("아이디를 입력하세요", text: $username)
+            }
+            InputView{
+                HStack{
+                    if shownPassword {
+                        SecureField("비밀번호를 입력하세요", text: $password)
+                        Button(action: {shownPassword.toggle()}, label: {
+                            Image(.iconEyeOff)
+                        })
+                    } else {
+                        TextField("비밀번호를 입력하세요", text: $password)
+                        Button(action: {shownPassword.toggle()}, label: {
+                            Image(.iconEyeOn)
+                        })
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 24)
+    }
+    
+    private var ButtonGroup: some View {
+        VStack(spacing: 24){
+            Button(action: {}, label: {
+                Text("로그인")
+                    .font(.SMedium)
+                    .foregroundStyle(.aricticLight)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 4)
+                            .foregroundStyle(.midnightDark)
+                    )
+            })
+            
+            HStack(spacing: 8) {
+                Rectangle()
+                    .fill(.aricticDark)
+                    .frame(height: 1)
+                    .frame(maxWidth: .infinity)
+                
+                Text("또는")
+                    .font(.SMedium)
+                    .foregroundStyle(.aricticDark)
+                
+                Rectangle()
+                    .fill(.aricticDark)
+                    .frame(height: 1)
+                    .frame(maxWidth: .infinity)
+            }
+            
+            Button(action: {}, label: {
+                Text("회원가입 하러 가기")
+                    .font(.SMedium)
+                    .foregroundStyle(.midnightDark)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(Color(.midnightDark), lineWidth: 1)
+                    )
+            })
+        }
+        .padding(.horizontal, 24)
     }
 }
 
